c41578b2ed10553cae1119d3fe9f3608ed6c07bc
yasirjasim <yalibadi@iu.edu>
2020-02-15 16:17:54 -0500

    Cloudmesh Exercises

--- cloudmesh-exercises/E.Cloudmesh.common.2.py
+++ cloudmesh-exercises/E.Cloudmesh.common.2.py
@@ -1,9 +1,16 @@
 from cloudmesh.common.dotdict import dotdict
 
 if __name__=="__main__":
-    person = {'name' :'Yasir',
-               'school':'IU'}
+    person = {'name' :'Yasir'}
 
     person = dotdict(person)
     print(f" Name: {person.name}")
-    print(f" School : {person.school}")
+
+    school = {'name': 'IU',
+              'capacity': '50,000',
+              'Location':'Bloomington'}
+
+
+    school = dotdict(school)
+    print(f" School Name: {school.name}")
+    print(f" School Capacity : {school.capacity}")
