780072a90213c7bbdd78acaa00aa505bc6b9515a
yasirjasim <yalibadi@iu.edu>
2020-04-04 14:00:57 -0400

    Create flavor and flavors functions

--- project/GoogleComputeService.py
+++ project/GoogleComputeService.py
@@ -1,12 +1,12 @@
 import os
-from pprint import pprint as pp
+from pprint import pprint
 
 import time
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
-# CLIENT_SECRET_FILE = '/e516/cm/sp20-516-223/project/prefab-manifest-269104-744f67274f4b.json'
-CLIENT_SECRET_FILE = '/e516/cm/<<HID>>/project/<<gcp_project>>-xxx.json'
+
+CLIENT_SECRET_FILE = '/Users/yasirjasim/.cloudmesh/google1.json'
 COMPUTE_API_SERVICE_NAME = 'compute'
 STORAGE_API_SERVICE_NAME = 'storage'
 COMPUTE_API_VERSION = 'v1'
@@ -14,28 +14,34 @@ COMPUTE_API_SCOPES = ['https://www.googleapis.com/auth/compute', 'https://www.go
 STORAGE_API_SCOPES = ['https://www.googleapis.com/auth/cloud-platform.read-only',
                       'https://www.googleapis.com/auth/devstorage.read_write']
 PROJECT_ID = 'cloudmesh-270102'
-BUCKET_NAME = 'cloudmesh-bucket'
-ZONE = 'us-east'
+ZONE = 'us-east1-b'
 MACHINE_TYPE = 'n1-standard-1'
-SERVICE_ACCOUNT_EMAIL = 'cloudmesh-service-account@prefab-manifest-269104.iam.gserviceaccount.com'
-START_UP_SCRIPT_PATH = '/e516/cm/sp20-516-235/project/'
-START_UP_SCRIPT_FILE = 'gcp_vm_startup_script.sh'
 
 def _get_credentials(client_secret_file, scopes):
     # Authenticate using service account.
+    print("start here- ------------------------------")
+    print(client_secret_file)
+    print(scopes)
     _credentials = service_account.Credentials.from_service_account_file(filename=client_secret_file,
-                                                                         scopes=scopes)
+                                                                             scopes=scopes)
     return _credentials
 
 
 def _get_compute_service(service_account_credentials):
     compute_service = None
+
+
+
     # Authenticate using service account.
     if service_account_credentials is None:
         print('Credentials are required')
     else:
-        compute_service = build(COMPUTE_API_SERVICE_NAME, COMPUTE_API_VERSION, credentials=service_account_credentials)
+        # print(service_account_credentials)
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_credentials
 
+        compute_service = build(COMPUTE_API_SERVICE_NAME, COMPUTE_API_VERSION)
+
+        # print(compute_service)
     return compute_service
 
 
@@ -49,27 +55,67 @@ def _get_storage_service(service_account_credentials):
 
     return storage_service
 
+def flavor(self, name):
+    """
+    Gets the flavor with a given name
+    :param name: The name of the flavor
+    :return: The dict of the flavor
+    """
+    comput_servce = _get_compute_service(CLIENT_SECRET_FILE)
+    flavor = _get_flavor(comput_servce, PROJECT_ID, ZONE, name)
+    return flavor
 
-def get_flavor(compute_service, flavor_project, family):
-    source_disk_flavor = None
-    # Get the images for the image project.
+def _get_flavor(compute_service, project_id, zone, name):
+    # Get the flavor for the project_id.
+    flavor = None;
     try:
-        flavor = compute_service.flavors().getFromFamily(project=flavor_project, family=family).execute()
-
+        flavor = compute_service.machineTypes().get(project=project_id, zone=zone, machineType=name).execute()
     except Exception as e:
         print(f'Error in get_flavors {e}')
     return flavor
 
 
-def get_flavors(compute_service, flavor_project):
+
+def flavors(self, **kwargs):
+    """
+    Lists the flavors on the cloud
+
+    :return: dict of flavors
+    """
+    comput_servce = _get_compute_service(CLIENT_SECRET_FILE)
+    return _get_flavors(comput_servce, PROJECT_ID)
+
+def _get_flavors(compute_service, project_id):
     source_disk_flavor = None
-    # Get the images for the image project.
+
+    # Get the flavors for the image project.
     try:
         # Get list of images related to image project.
-        flavor_response = compute_service.flavors().list(project=flavor_project).execute()
+        flavor_response = compute_service.machineTypes().aggregatedList(project=project_id).execute()
         # Extract the items.
         source_disk_flavor = flavor_response['items']
-
+        # print('flavors 2')
     except Exception as e:
         print(f'Error in get_flavors {e}')
     return source_disk_flavor
+
+if __name__ == "__main__":
+
+    print('flavor1 :')
+    pprint(flavor(MACHINE_TYPE, name='n1-standard-1'))
+
+    print('flavor2 :')
+    pprint(flavor(MACHINE_TYPE, name='n1-standard-2'))
+
+    print('flavors')
+    pprint(flavors(PROJECT_ID))
+
+
+
+
+
+
+
+
+
+
